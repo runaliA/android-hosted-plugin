b30aadbe6685299cf464b93351ef84c79e995ccd
Runali Ambavkar <runali.ambavkar@concerto.in>
2026-07-27 01:00:40 +0530

    Maven Central Publish

---
+++ build/generated/source/buildConfig/release/com/data/hostedpayments/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.data.hostedpayments;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.data.hostedpayments";
+  public static final String BUILD_TYPE = "release";
+  // Field from build type: release
+  public static final String SDK_VERSION_CODE = "v3.0.3";
+}
---
+++ build/intermediates/java_doc_dir/release/javaDocReleaseGeneration/module-search-index.js
@@ -0,0 +1,1 @@
+var moduleSearchIndex = [{"l":":android_hosted_plugin","url":"index.html"}]
